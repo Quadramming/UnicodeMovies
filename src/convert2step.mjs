baf45e6476370d1de0d6d3e76741d90b2c08627b
Creature <creature@Creatures-Mac-mini.local>
2019-03-10 23:15:37 +0300

    Work

--- src/convert2step.mjs
+++ src/convert2step.mjs
@@ -3,7 +3,6 @@ import movies from './movies_converter_tmp.mjs';
 
 fs.writeFileSync('./src/movies_converter_tmp.mjs', JSON.stringify(movies));
 let data = fs.readFileSync('./src/movies_converter_tmp.mjs');
-data = data.toString().replace(/hash\.mjs/g, 'hash.js');
 data = data.toString().replace(/\\\\u/g, '\\u');
 fs.writeFileSync('./src/movies.js', 'export default '+data);
 fs.unlinkSync('./src/movies_converter_tmp.mjs');
